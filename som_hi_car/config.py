ac2aa3aff43eb2ae6d2376189f13e2eef31097a1
Santi Sabate <ssabate@iesebre.com>
2025-05-25 19:55:13 +0200

    Config per postgresql

--- som_hi_car/config.py
+++ som_hi_car/config.py
@@ -2,8 +2,9 @@ import os
 import secrets
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'oracle+oracledb://SOMHI:SOMHI@localhost:1521/xe'
+    #SQLALCHEMY_DATABASE_URI = 'oracle+oracledb://SOMHI:SOMHI@localhost:1521/xe'
     #SQLALCHEMY_DATABASE_URI = 'oracle+cx_oracle://SOMHI:somhi@localhost:1521/xe'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://somhicar:somhicar@localhost:5434/somhicar'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     UPLOAD_FOLDER = os.path.join('static', 'images', 'avatars')
 
