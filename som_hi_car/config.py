82ea38830cc7174b2ad323b16c0937255670532e
Santi Sabate <ssabate@iesebre.com>
2025-05-31 02:37:22 +0200

    Actualitzo config

--- som_hi_car/config.py
+++ som_hi_car/config.py
@@ -2,15 +2,15 @@ import os
 import secrets
 
 class Config:
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://somhicar:kSUjkeAgemEvXBVkPwMXKCi5OdBskbPT@dpg-d0pluvumcj7s73ea9jpg-a/somhicar'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://somhicar:somhicar@localhost:5434/somhicar'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://somhicar:kSUjkeAgemEvXBVkPwMXKCi5OdBskbPT@dpg-d0pluvumcj7s73ea9jpg-a/somhicar'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://somhicar:somhicar@localhost:5434/somhicar'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     UPLOAD_FOLDER = os.path.join('static', 'images', 'avatars')
 
     # Mostra consultes SQL per depurar
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
 
     # En producció a false
-    DEBUG = True
+    DEBUG = False
 
     SECRET_KEY = secrets.token_hex(16)  # Genera una clau secreta aleatòria de 32 caràcters hexadecimals
